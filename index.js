e48d77d25aa7b9d1972e3cda1d577899a1ba0670
iarleisouza <iarlei.souza@gmail.com>
2023-09-12 14:39:59 -0300

    subindo projeto

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 
-
 function outcome() {
+    let num1 = Number(document.getElementById('num-one').value);
+    let num2 = Number(document.getElementById('num-two').value);
+    
 
-    let num1 = Number(document.getElementById('num-one').value)
-    let num2 = Number(document.getElementById('num-two').value)
-    let total = 0
+     let input1 = document.getElementById('box1').checked;
+     let input2 = document.getElementById('box2').checked;
+     let input3 = document.getElementById('box3').checked;
+     let input4 = document.getElementById('box4').checked;
+     let result = document.getElementById('resultArea');
+   
 
-    if (document.getElementById('box1').checked)
-        total = num1 + num2
-    else if (document.getElementById('box2').checked)
-        total = num1 - num2
-    else if (document.getElementById('box3').checked)
-        total = num1 * num2
-    else
-        total = num1 / num2
-    document.getElementById('resultArea').innerHTML = 'Result: ' + String(total)
+     if (num1 == '' && num2 == ''){
+        alert('Digite um número!')
+        result = '';
+       
+     } else if(input1) {
+         result = num1 + num2;
+      
+     }else if (input2){
+         result = num1 - num2;
+
+     }else if(input3){
+        result = num1 * num2;
+     } else if(input4) {
+        result = num1 / num2;
+       
+     }
+     document.getElementById('resultArea').innerHTML = result;
 }
